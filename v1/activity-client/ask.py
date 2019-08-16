938f30ac27410d85045e86fd47f2527fdfcccdc4
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-08-16 13:24:44 +0530

    Added basic time restirction

--- v1/activity-client/ask.py
+++ v1/activity-client/ask.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
+import sys
+
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-from util import question
+from util import question, util
 
 # Create .env file path.
 dotenv_path = join(dirname(__file__), '.env')
 
 # Load file from the path.
 load_dotenv(dotenv_path)
+
+def is_not_allowed(from_hour, from_min, to_hour, to_min, message):
+    if util.is_within(from_hour, from_min, to_hour, to_min):
+        util.banner(f'It is {message}. Please shutdown the computer.')
+        sys.exit(0)
+
+is_not_allowed(15, 30, 16, 30, 'STUDY TIME')
 
 questions = question.get()
 question.ask(questions)
--- v1/activity-client/util/util.py
+++ v1/activity-client/util/util.py
@@ -3,6 +3,7 @@ import re
 import os
 import requests
 import json
+import datetime
 
 def repeat(c, n):
     r = ""
@@ -27,3 +28,10 @@ def post_activity(response):
     except:
         print("Internet connection is down. Please check the Wifi modem!")
         sys.exit(1)
+
+def is_within(from_hour, from_min, to_hour, to_min):
+    now = datetime.datetime.now()
+    current_date = now.strftime('%Y-%m-%d-%H:%M:%S')
+    from_date = datetime.datetime(now.year, now.month, now.day, from_hour, from_min).strftime('%Y-%m-%d-%H:%M:%S')
+    to_date = datetime.datetime(now.year, now.month, now.day, to_hour, to_min).strftime('%Y-%m-%d-%H:%M:%S')
+    return current_date >= from_date and current_date <= to_date
