3b5123395266b63760ce556b581e9ec4b7959289
Dipanjan Bhowmik <onlydevelop@gmail.com>
2020-03-06 10:48:05 +0530

    Added checking of correct answer

--- v3/src/Question.js
+++ v3/src/Question.js
@@ -18,8 +18,13 @@ export default class Question extends React.Component {
             subject: "",
             topic: "",
             question: "",
-            options: []
+            options: [],
+            correct: "",
+            response: "",
+            feedback: ""
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.validateAnswer = this.validateAnswer.bind(this);
     }
 
     static getRandomInt(max) {
@@ -35,23 +40,43 @@ export default class Question extends React.Component {
                 var topics = subject["topics"][Question.getRandomInt(subject["topics"].length)]
                 var questions = topics["questions"][Question.getRandomInt(topics["questions"].length)]
                 var options = questions["answer"]["options"]
+                var correct = questions["answer"]["correct"]
 
                 console.log("subject = " + subject["subject"])
                 console.log("topic = " + topics["topic"])
                 console.log("question = " + questions["question"])
                 console.log("options = " + options)
+                console.log("correct = " + correct)
 
                 return {
                     subject: subject["subject"],
                     topic: topics["topic"],
                     question: questions["question"],
-                    options: options
+                    options: options,
+                    correct: correct,
+                    response: "",
+                    feedback: ""
                 }
             }
         }
         return null;
     }
     
+    handleChange(event) {
+        this.setState({response: event.target.value});
+    }
+
+    validateAnswer() {
+        let feedback = "";
+        if(this.state.response === this.state.correct) {
+            feedback = "You are orrect!";
+        } else {
+            feedback = "Correct answer: " + this.state.correct;
+        }
+
+        this.setState({feedback: feedback});
+    }
+
     render() {
         return (
             <div class="card">
@@ -63,6 +88,11 @@ export default class Question extends React.Component {
                         {this.state.options.map(option => <li>{option}</li>)}
                     </ol>
                 </div>
+                <div class="form-inline">
+                    <input type="text" class="form-control mb-2 mr-sm-2" value={this.state.response} onChange={this.handleChange}/>
+                    <button type="submit" class="btn btn-secondary mb-2" onClick={this.validateAnswer}>Check</button>
+                </div>
+        <div class="card-footer">{this.state.feedback}</div>
               </div>
             </div>
         )
